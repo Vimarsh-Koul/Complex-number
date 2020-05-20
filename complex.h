672a832e9d6b45bf3b4d3c4817979933e455980c
Vimarsh-Koul <vimarsh2001koul@gmail.com>
2020-05-20 14:31:17 +0530

    Included addition and subtraction

--- complex.h
+++ complex.h
@@ -15,30 +15,6 @@ public:
 	complex operator-(complex);
 	complex operator*(complex);
 };
-complex complex::operator +(complex c)
-{
-	complex sol;
-	sol.real = real+c.real;
-	sol.imag = imag+c.imag;
-	return sol;
-}
-complex complex::operator -(complex c)
-{
-	complex sol;
-	sol.real = real-c.real;
-	sol.imag = imag-c.imag;
-	return sol;
-}
-complex complex::operator *(complex c)
-{
-	complex sol;
-	sol.real = real*c.real - imag*c.imag;
-	sol.imag = real*c.imag + c.real*imag;
-	return sol;
-}
-std::ostream &operator<<(std::ostream &output, const complex c){
 
-std::cout<<"Your number is "<<"("<<c.real<<")"<<" + "<<"("<<c.imag<<")"<<"i"<<std::endl;
-}
 
 #endif
